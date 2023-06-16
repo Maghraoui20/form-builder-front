6bce64ff9e2264d4988afea55452dc5459822322
ameni maghraoui <maghraouiameni20@gmail.com>
2023-06-16 13:37:19 +0100

    fix-home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,7 @@ function Home() {
   
   return (
 <div>
-    <div style={{display:"flex" , justifyContent:"center" , margin:50}}>
+    <div style={{display:"flex" , justifyContent:"center" , margin:50 , fontSize:25}}>
       <div >
         <h2>Home</h2>
         <p>Welcome to Page Home</p>
